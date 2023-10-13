f69030f270cbc73fad1d691aac0a31b24b786912
Geovanny  Gómez Leiva <geovanny.gomez@pandatechla.com>
2023-10-13 11:52:16 -0600

    Problem solution

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export const handler = async (event) => {
+exports.handler = async (event) => {
     // Verificar si se proporciona un encabezado de autorización
     const authorizationHeader = event.headers.Authorization;
     
